89319937e01e9352389cbbe10b630f0df5fca50b
Pame <pgendrop@anahuac.mx>
2025-05-12 19:22:25 -0600

    Refactor comments for clarity and consistency in servers.js

--- Backend/servers.js
+++ Backend/servers.js
@@ -2,30 +2,36 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDB = require("./config/db");
-const cors = require('cors');
+const cors = require("cors");
 
-dotenv.config(); // Cargar variables de entorno
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
 
+// Inicializar Express
 const app = express();
+
+// Usar el puerto asignado por Render o el 8000 por defecto
 const PORT = process.env.PORT || 8000;
 
-connectDB(); // Conectar a MongoDB
+// Conectar a MongoDB
+connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Registrar rutas
+// Rutas API
 app.use("/api/habitaciones", require("./routes/habitacionesRoutes"));
 app.use("/api/reservaciones", require("./routes/reservacionesRoutes"));
 app.use("/api/usuarios", require("./routes/userRoutes"));
 
-// Ruta principal
+// Ruta principal para comprobar funcionamiento
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-// Iniciar el servidor
+// Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`.yellow.bold);
 });
